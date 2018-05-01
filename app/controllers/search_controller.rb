3cd875dd5c23f22ee63ac71401b00b185ad5f15f
kylesallette <kylesallette@gmail.com>
2018-05-01 11:36:17 -0600

    adding to seach controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,19 +1,9 @@
 class SearchController < ApplicationController
 
   def index
-    state = params[:state]
-    @conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-      faraday.headers["X-API-KEY"] = "S9JON3ruNOI6XiyymcnZ7gtsjnToPxuXyT0bgeaX"
-      faraday.adapter Faraday.default_adapter
-    end
-    response = @conn.get("/congress/v1/members/house/#{state}/current.json")
-    @members = JSON.parse(response.body, symbolize_names: true)[:results]
-
-    results = JSON.parse(response.body, symbolize_names: true)[:results]
-
-    @members  = results.map do |result|
-     Member.new(result)
+    @members = Member.find_all(params[:state])
   end
-end
 
-end 
+
+
+end
