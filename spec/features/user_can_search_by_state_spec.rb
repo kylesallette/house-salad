1e0095e6ca8f1f1697cc0ba3f01deb9b9df31364
kylesallette <kylesallette@gmail.com>
2018-05-01 12:15:01 -0600

    adding feature test

--- spec/features/user_can_search_by_state_spec.rb
+++ spec/features/user_can_search_by_state_spec.rb
@@ -1,23 +1,33 @@
 require 'rails_helper'
 
-  feature "user can search for house members" do
+feature "user can search for house members" do
 
-    scenario "user submits valid state name"  do
+  scenario "user submits valid state name" do
+    # As a user
+    # When I visit "/"
+    visit '/'
 
-      visit '/'
-
+    # And I select "Colorado from the dropdown
     select "Colorado", from: :state
 
-    click_on "Locate Members of the House"
+    # And I click on Locate Members of the house"
+    VCR.use_cassette("features/user_can_search_by_state") do
+      click_on "Locate Members of the House"
 
-    expect(page).to have_content("7 Results")
-    expect(page).to have_css(".member", count: 7)
+      # Then my path should be "/search" with "state=CO" in the params
+      expect(current_path).to eq(search_path)
 
-    within(first(".member")) do
-      expect(page).to have_css(".name")
-      expect(page).to have_css(".role")
-      expect(page).to have_css(".party")
-      expect(page).to have_css(".district")
+      # And I should see a message "7 results"
+      expect(page).to have_content("7 Results")
+
+      # And i should see name, role, party, district
+      within(first(".member")) do
+        expect(page).to have_css(".name")
+        expect(page).to have_css(".role")
+        expect(page).to have_css(".party")
+        expect(page).to have_css(".district")
+      end
     end
   end
-end 
+
+end
