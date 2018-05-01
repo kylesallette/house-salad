a6d1f2c99a683f1f3519133bf87bd3d36f99d260
kylesallette <kylesallette@gmail.com>
2018-05-01 12:15:47 -0600

    adding member model

--- app/models/member.rb
+++ app/models/member.rb
@@ -12,21 +12,4 @@ class Member
     @district   = attributes[:district]
     @seniority  = attributes[:seniority].to_i
   end
-
-  def find_all(state)
-
-    @conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-      faraday.headers["X-API-KEY"] = ENV["propublica_key"]
-      faraday.adapter Faraday.default_adapter
-    end
-
-    response = @conn.get("/congress/v1/members/house/#{state}/current.json")
-
-    results = JSON.parse(response.body, symbolize_names: true)[:results]
-
-    @members  = results.map do |result|
-      Member.new(result)
-    end
-  end
-
 end
