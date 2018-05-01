8bbc97167d2dea1adc84ab40e47bbb99f464bf65
kylesallette <kylesallette@gmail.com>
2018-04-30 20:23:12 -0600

    project working

---
+++ spec/features/user_can_search_by_state_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+  feature "user can search for house members" do
+
+    scenario "user submits valid state name"  do
+
+      visit '/'
+
+    select "Colorado", from: :state
+
+    click_on "Locate Members of the House"
+
+    expect(page).to have_content("7 Results")
+    expect(page).to have_css(".member", count: 7)
+
+    within(first(".member")) do
+      expect(page).to have_css(".name")
+      expect(page).to have_css(".role")
+      expect(page).to have_css(".party")
+      expect(page).to have_css(".district")
+    end
+  end
+end 
